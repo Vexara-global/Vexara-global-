9f2ed332f70c7967ce7952e26c8a8ee8f6ea5e67
Vexara-global <vexaraglobal@gmail.com>
2026-02-03 19:03:14 +0100

    Delete Js directory

--- Js/main.js
+++
@@ -1,6 +0,0 @@
-// js/main.js
-   const toggle = document.getElementById('menuToggle');
-   const menu = document.getElementById('navMenu');
-   if (toggle && menu) {
-     toggle.addEventListener('click', () => menu.classList.toggle('active'));
-   }
