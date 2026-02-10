e598636b4ac4bed6ec542ec810b5e01efdbfe20e
Vexara-global <vexaraglobal@gmail.com>
2026-02-10 20:16:35 +0100

    Update messages.js

--- netlify/functions/messages.js
+++ netlify/functions/messages.js
@@ -6,12 +6,24 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }
 });
 
-exports.handler = async () => {
+exports.handler = async (event) => {
   try {
-    const res = await pool.query('SELECT * FROM messages ORDER BY submitted_at DESC');
-    return { statusCode: 200, body: JSON.stringify(res.rows) };
+    const method = event.httpMethod;
+
+    if (method === 'GET') {
+      const res = await pool.query('SELECT * FROM messages ORDER BY submitted_at DESC');
+      return { statusCode: 200, body: JSON.stringify(res.rows) };
+    }
+
+    if (method === 'DELETE') {
+      const data = JSON.parse(event.body);
+      await pool.query('DELETE FROM messages WHERE id = $1', [data.id]);
+      return { statusCode: 200, body: '{}' };
+    }
+
+    return { statusCode: 405, body: 'Method not allowed' };
   } catch (err) {
     console.error(err);
-    return { statusCode: 500, body: JSON.stringify({ error: "Failed to load messages" }) };
+    return { statusCode: 500, body: JSON.stringify({ error: "Server error" }) };
   }
 };
